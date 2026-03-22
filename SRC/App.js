1cc3c694f2a23ecd9e814b637654af1cc3510466
carlosbonfim <carlosbonfim722@gmail.com>
2026-03-22 19:29:48 -0300

    config: correção de erro de import

--- SRC/App.js
+++ SRC/App.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
-import router from "./routes/route.js";
+import router from "./routes/Route.js";
 
 dotenv.config();
 
